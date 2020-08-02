bfd64c4b37c11a2f6b0ac228c9290cb06549199e
Magda <makneta@gmail.com>
2020-08-02 23:47:37 +0200

    Updates

---
+++ precache-manifest.1eb7f824c5e897b2be9c8aa53e35f4f0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4152e1253576c11d8d69fbd3e37e8ea2",
+    "url": "/product-cards/index.html"
+  },
+  {
+    "revision": "d5fa05ca3cbe473e7dfc",
+    "url": "/product-cards/static/css/main.d07630e8.chunk.css"
+  },
+  {
+    "revision": "6b0927115bceab3a1cbd",
+    "url": "/product-cards/static/js/2.9eb6a52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-cards/static/js/2.9eb6a52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fa05ca3cbe473e7dfc",
+    "url": "/product-cards/static/js/main.bcec5f4b.chunk.js"
+  },
+  {
+    "revision": "9e90ca9c40bb8496a453",
+    "url": "/product-cards/static/js/runtime-main.57c3787a.js"
+  },
+  {
+    "revision": "b7de00d07e5a0585f39f38ef7cf23d31",
+    "url": "/product-cards/static/media/boho-bangle-bracelet.b7de00d0.jpg"
+  },
+  {
+    "revision": "87315860ba83ada79a601d739646646a",
+    "url": "/product-cards/static/media/chakra-bracelet.87315860.jpg"
+  },
+  {
+    "revision": "5ee0027165d17e7c0fa9bd26bd90ea84",
+    "url": "/product-cards/static/media/embroidered-bracelet.5ee00271.jpg"
+  },
+  {
+    "revision": "99ba0ffe1c03635cdcebb0569f89cea3",
+    "url": "/product-cards/static/media/stacked-bracelets-set.99ba0ffe.jpg"
+  },
+  {
+    "revision": "5c744f392b3a70ffe4da1576e611da71",
+    "url": "/product-cards/static/media/stacked-bracelets.5c744f39.jpg"
+  }
+]);
